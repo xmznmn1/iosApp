a3990e1e42a919d63d6585d687b4f4ec0f5b9168
xmznmn1 <meng.xin@puzzleplusgames.com>
2021-04-25 19:33:12 +0800

    cat plist

--- web/plist.py
+++ web/plist.py
@@ -4,6 +4,7 @@
 from biplist import *
 from datetime import datetime
 import sys
+import os
 
 
 url = sys.argv[1]
@@ -32,9 +33,12 @@ plist = {
 try:
     if env == 'production':
         writePlist(plist, "manifest.plist", binary=False)
+        os.system('cat manifest.plist')
     elif env == 'sandbox':
         writePlist(plist, "sandboxfest.plist", binary=False)
+        os.system('cat sandboxfest.plist')
     elif env == 'development':
         writePlist(plist, "developmentfest.plist", binary=False)
+        os.system('cat developmentfest.plist')
 except(InvalidPlistException, NotBinaryPlistException) as e:
     print("Something bad happened:", e)
