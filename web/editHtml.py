1b6825fde292bc5f486312ecd91457b8521e2b1c
xmznmn1 <meng.xin@puzzleplusgames.com>
2021-04-27 16:53:51 +0800

    (attrs='instructions')[1]

--- web/editHtml.py
+++ web/editHtml.py
@@ -14,8 +14,8 @@ if env == 'production':
     soup.find_all(attrs='instructions')[0].string= '安装production环境包：'+ipaName
     print(soup.find_all(attrs='instructions')[0].string)
 elif env == 'sandbox':
-    soup.find_all(attrs='instructions')[2].string = '安装sandbox环境包：'+ipaName
-    print(soup.find_all(attrs='instructions')[2].string)
+    soup.find_all(attrs='instructions')[1].string = '安装sandbox环境包：'+ipaName
+    print(soup.find_all(attrs='instructions')[1].string)
 elif env == 'development':
     soup.find_all(attrs='instructions')[2].string = '安装development环境包：'+ipaName
     print(soup.find_all(attrs='instructions')[2].string)
